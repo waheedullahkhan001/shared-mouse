b1329dca97d1872e38d7e09c4080917e84c823d4
waheedullahkhan <waheedullahkhan1250@gmail.com>
2022-07-31 13:35:22 +0500

    simple GUI in PyQt5

--- gui.py
+++ gui.py
@@ -1,17 +1,38 @@
-import tkinter as tk
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLineEdit, QPushButton, QLabel
 
 
-class App(tk.Tk):
-
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Shared Mouse")
+        self.setFixedSize(400, 80)
+        self.createWidgets()
+        self.show()
 
-        # configure the root window
-        self.title('Mouse Without Borders')
-        self.resizable(False, False)
+    def createWidgets(self):
+        self.grid = QGridLayout()
+        self.ipLineEdit = QLineEdit()
+        self.joinButton = QPushButton("Join")
+        self.hostButton = QPushButton("Host")
+        self.statusLable = QLabel("Status: Waiting for user input...")
+
+        self.joinButton.clicked.connect(self.joinButtonClicked)
+
+        self.grid.addWidget(self.ipLineEdit, 0, 0, 1, 2)
+        self.grid.addWidget(self.joinButton, 0, 2)
+        self.grid.addWidget(self.hostButton, 0, 3)
+        self.grid.addWidget(self.statusLable, 1, 0, 1, 4)
+        
+        self.setLayout(self.grid)
+
+    def joinButtonClicked(self):
+        pass
 
 
+app = QApplication(sys.argv)
 
-if __name__ == '__main__':
-    app = App()
-    app.mainloop()
+window = MainWindow()
+
+sys.exit(app.exec_())
