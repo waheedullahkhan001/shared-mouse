fb8e6cc1768577757d4c0fedad727d755cc25826
Naveed Khan <m.naveed.u.khan.n@gmail.com>
2022-08-01 05:00:39 +0500

    changes for some host and join button & EH

--- gui.py
+++ gui.py
@@ -15,19 +15,25 @@ class MainWindow(QWidget):
         self.setFixedSize(400, 80)
         self.createWidgets()
         self.show()
+        self.server = None
+        self.client = None
+        self.hostThread = None
+        self.clientThread = None
 
     def createWidgets(self):
         self.grid = QGridLayout()
 
-        self.ipLineEdit = QLineEdit()
+        self.addressTextField = QLineEdit()
         self.joinButton = QPushButton("Join")
         self.hostButton = QPushButton("Host")
         self.statusLable = QLabel("Status: Waiting for user input...")
+        self.hosting = False
+        self.joining = False
 
         self.joinButton.clicked.connect(self.joinButtonClicked)
         self.hostButton.clicked.connect(self.hostButtonClicked)
 
-        self.grid.addWidget(self.ipLineEdit, 0, 0, 1, 2)
+        self.grid.addWidget(self.addressTextField, 0, 0, 1, 2)
         self.grid.addWidget(self.joinButton, 0, 2)
         self.grid.addWidget(self.hostButton, 0, 3)
         self.grid.addWidget(self.statusLable, 1, 0, 1, 4)
@@ -35,19 +41,51 @@ class MainWindow(QWidget):
         self.setLayout(self.grid)
 
     def joinButtonClicked(self):
-        self.disableControlls()
-        self.setStatus("Connecting...")
-
-        client = SharedMouseClient(self.ipLineEdit.text(), 8901)
-        self.setStatus("Connected to server!")
-
-        Thread(target=client.clientLoop, daemon=True).start()
+        # self.disableControlls()
+        if not self.joining:
+            address = self.addressTextField.text()
+            if address == "":
+                address = "localhost"
+            try:
+                self.setStatus("Connecting...")
+                self.client = SharedMouseClient(address, 8901)
+                self.setStatus("Connected to server!")
+                self.clientThread = Thread(target=self.client.clientLoop, daemon=True)
+                self.clientThread.start()
+                self.joining = True
+                self.joinButton.setText("Stop")
+                self.hostButton.setEnabled(False)
+                self.addressTextField.setEnabled(False)
+            except Exception as e:
+                self.setStatus("Something went wrong while connecting to server!")
+        else:
+            # kill clientThread here & test if it works
+            self.client.close()
+            self.setStatus("Waiting for user input...")
+            self.joinButton.setText("Join")
+            self.joinButton.setEnabled(True)
+            self.hostButton.setEnabled(True)
+            self.addressTextField.setEnabled(True)
+            self.joining = False
 
     def hostButtonClicked(self):
-        self.disableControlls()
-        server = SharedMouseServer('0.0.0.0', 8901)
-        Thread(target=self.waitClient, args=(server,), daemon=True).start()
-        self.setStatus("Waiting for client...")
+        if not self.hosting:
+            self.joinButton.setEnabled(False)
+            self.addressTextField.setEnabled(False)
+            self.server = SharedMouseServer('0.0.0.0', 8901)
+            self.hostThread = Thread(target=self.waitClient, args=(self.server,), daemon=True)
+            self.hostThread.start()
+            self.setStatus("Waiting for client...")
+            self.hostButton.setText("Stop")
+            self.hosting = True
+        else:
+            # kill hostThread here & test if it works
+            self.server.close()
+            self.setStatus("Waiting for user input...")
+            self.joinButton.setEnabled(True)
+            self.addressTextField.setEnabled(True)
+            self.hostButton.setText("Host")
+            self.hosting = False
 
     def waitClient(self, server: SharedMouseServer):
         server.waitForClient()
@@ -58,7 +96,7 @@ class MainWindow(QWidget):
         self.statusLable.setText(f"Status: {text}")
 
     def disableControlls(self):
-        self.ipLineEdit.setEnabled(False)
+        self.addressTextField.setEnabled(False)
         self.joinButton.setEnabled(False)
         self.hostButton.setEnabled(False)
 
